a5dd961f5a8a635675bbfec479f8a24c5483dcd0
namancodeeess <nikku22116@gmail.com>
2026-08-18 16:31:25 +0530

    Time: 0 ms (100%), Space: 30.1 MB (99.15%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,98 +1,24 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& a) {
-
-        int siz = a.size();
-
-        vector<int> neg;
-        vector<int> pos;
-
-        for(int k = 0; k < siz; k++)
-        {
-            if(a[k] < 0)
-                neg.push_back(a[k]);
-            else
-                pos.push_back(a[k]);
-        }
-
-        if(neg.size() == 0)
-        {
-            for(int k = 0; k < pos.size(); k++)
-                pos[k] = pos[k] * pos[k];
-
-            return pos;
-        }
-
-        if(pos.size() == 0)
-        {
-            for(int k = 0; k < neg.size(); k++)
-                neg[k] = neg[k] * neg[k];
-
-            reverse(neg.begin(), neg.end());
-
-            return neg;
-        }
-
-
-        int n = neg.size();
-        int m = pos.size();
-
-        vector<int> res(n + m);
-
-
-        // square negative
-        for(int k = 0; k < n; k++)
-            neg[k] = neg[k] * neg[k];
-
-
-        // reverse negative part
-        reverse(neg.begin(), neg.end());
-
-
-        // square positive
-        for(int k = 0; k < m; k++)
-            pos[k] = pos[k] * pos[k];
-
-
-        int i = 0;
-        int j = 0;
-        int id = 0;
-
-
-        // merge two sorted arrays
-        while(i < n && j < m)
-        {
-            if(neg[i] <= pos[j])
-            {
-                res[id] = neg[i];
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(n);
+        int i=0;
+        int j=n-1;
+        int k=n-1;
+        while(i<=j){
+            if(abs(nums[i])>abs(nums[j])){
+                ans[k]=nums[i]*nums[i];
                 i++;
             }
-            else
-            {
-                res[id] = pos[j];
-                j++;
+            else{
+                ans[k]=nums[j]*nums[j];
+                j--;
             }
-
-            id++;
+            k--;
         }
+        return ans;
 
-
-        while(i < n)
-        {
-            res[id] = neg[i];
-            i++;
-            id++;
-        }
-
-
-        while(j < m)
-        {
-            res[id] = pos[j];
-            j++;
-            id++;
-        }
-
-
-        return res;
+        
     }
 };
